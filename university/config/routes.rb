819fe59b0d829302f0e4552769e87868bfe80868
Myde Rojas <mydelyn.rojas@outlook.com>
2018-10-04 21:35:53 -0600

    Created a Welcome Page for University

--- university/config/routes.rb
+++ university/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :students
   resources :courses
   resources :sections
+
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
